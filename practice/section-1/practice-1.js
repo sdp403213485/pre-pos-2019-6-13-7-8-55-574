adfd63e228948e07a0ed7520954fb739de9a2f90
sdp403213485 <45375846+sdp403213485@users.noreply.github.com>
2019-08-19 22:03:52 +0800

    Update practice-1.js

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,16 @@
 'use strict';
 
-function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+function collectSameElements(collectionA, collectionB){ 
+var c = new Array(); 
+for(var i = 0; i<collectionA.length ; i++) 
+{ 
+	for(var j = 0; j<collectionB.length ; j++) 
+	{ 
+		if (collectionA[i] == collectionB[j]) 
+		{ c.push(collectionA[i]); 
+			break; 
+		} 
+	} 
+} 
+	return c;
 }

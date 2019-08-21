11d83eeab9d8f7b4e84233c52406fd491e7432cb
sdp <403213485@qq.com>
2019-08-21 10:42:58 +0800

    21

---
+++ practice/section-1/practice-2 - 副本.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function collectSameElements(collectionA, collectionB){
+	var c = new Array(); 
+	collectionB=collectionB[0]; 
+	for(var i = 0; i<collectionA.length ; i++){		
+		for(var j = 0; j<collectionB.length ; j++) {
+			if (collectionA[i] == collectionB[j]) { 
+			c.push(collectionA[i]); 
+			break; 
+			}
+		} 
+	}	 
+return c; 
+}
--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,18 +1,17 @@
 'use strict';
 
-function collectSameElements(collectionA, collectionB)
- { var c = new Array(); 
- collectionB=collectionB[0]; 
- for(var i = 0; i<collectionA.length ; i++) 
- { 
-	for(var j = 0; j<collectionB.length ; j++) 
-	{
-	 if (collectionA[i] == collectionB[j]) 
-	{ 
-		c.push(collectionA[i]); 
-		break; 
+function collectSameElements(collectionA, collectionB){
+	var result = [];
+	collectionB=collectionB[0]; 
+	for(var i = 0;i<collectionA.length;i++){
+		for(var j = 0;j<collectionB.length;j++){
+			if (collectionA[i] == collectionB[j]) { 
+			result.push(collectionA[i]); 
+			break; 
+			}
+		}
+			
 	}
-	} 
-} 
-return c; 
+	return result; 
 }
+
